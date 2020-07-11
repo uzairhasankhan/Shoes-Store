d4205a84f51b9733df36bd111b78b1eaface90c2
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-07-12 01:48:05 +0500

    Logo Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <nav>
+      <Link to="/">Home</Link>
+
+      <Link to="/launch">Launch</Link>
+    </nav>
+
+  
+    <Routes>
+      <Route path="/" element ={<Home />} />
+      <Route path="/launch" element ={<Launch />}>
+      <Route path="/" element={<LaunchIndex />} />
+      <Route path=":slug" element={<LaunchShoe />} />
+
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+  <div>
+    <h1> Welcome Home</h1>
+  </div>
+
+  );
+}
+
+function Launch() {
+  return (
+  <div className=".App-logo">
+    <h1> Welcome Launch</h1>
+    <Outlet />
+  </div>
+
+  );
+}
+
+function LaunchShoe() {
+  const {slug} = useParams();
+  const shoe = shoes[slug];
+  if (!shoe) {
+    return <h2>Not Found</h2>
+  }
+
+  const { name, img } = shoe;
+
+  return (
+     <div>
+       <h2>{name}</h2>
+       <img src={img} alt={name} />
+  </div>
+  );
+}
+
+function LaunchIndex () {
+  return (
+    <ul>
+      {Object.entries(shoes).map(([slug, { name, img }]) => (
+        <li key={slug}>
+          <Link to={`/launch/${slug}`}>
+          <h2>{name}</h2>
+          <img src={img} alt={name} />
+          </Link>
+          
+          </li>
+      ))}
+    </ul>
+
+
+  );
+}
+
+
+const shoes = {
+  "air-jordan-3-valor-blue": {
+    name: "VALOUR BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "jordan-mars-270-london": {
+    name: "JORDAN MARS 270 LONDON",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "air-jordan-1-zoom-racer-blue": {
+    name: "RACER BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  }
+};
